1689af975e7a9d4990afe06edcda5fbb42d0ba26
giegeralexandra <giegeralexandra@gmail.com>
2021-05-22 17:36:02 -0400

    downloaded middleware

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
 
-function App() {
+export default class App extends React.Component {
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/rentals')
+  //   .then(res => res.json())
+  //   .then(rentals => console.log(rentals[1].reservations))
+  // }
+
+  
+  
+  render(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App
     </div>
-  );
+    
+  )
+  }
 }
 
-export default App;
