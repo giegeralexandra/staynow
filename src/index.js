b6958e97dab21efddf08a0114b29a84cef541e97
giegeralexandra <giegeralexandra@gmail.com>
2021-05-22 18:31:32 -0400

    set up devtools

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
-
 import App from './App';
+import rentalReducer from './reducers/rentalReducer'
+
 
 //compose combines diff middlewares into one 
 //store is where you are storing your data locally
@@ -12,7 +13,7 @@ import App from './App';
 const composeEnhancers = ((window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose)
 let reducer = ''
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(rentalReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   //Provider allows store global to all other containers we create, any component in provider will have access to store we pass in provider
@@ -23,6 +24,11 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
+
+
+
+//container component usually class components and render other components and contain other components
+//the container folder specifically renders other components
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/reducers/rentalReducer.js
@@ -0,0 +1,7 @@
+//reducer is a place that defines action types - takes in previous state, action type, all it really is is a function 
+
+export default function rentalReducer(state = {rentals: []}, action){
+
+    return state
+
+}
