d4657ab873a0fd6f80d220f277d618ecd2221386
giegeralexandra <giegeralexandra@gmail.com>
2021-05-22 21:23:15 -0400

    updated fetchRentals and RentalsContainer

--- src/actions/fetchRentals.js
+++ src/actions/fetchRentals.js
@@ -1,12 +1,15 @@
 
 
-export function fetchRentals(action) {
-
-    return action
-    // fetch('http://localhost:3000/api/v1/rentals')
-    // .then(res => res.json())
-    // .then(rentals => console.log(rentals))
-
+export function fetchRentals() {
+    return (dispatch) => {
+        console.log('inside fetch rentals')
+        fetch('http://localhost:3000/api/v1/rentals')
+        .then(res => res.json())
+        .then(rentals => dispatch({
+            type: 'FETCH_RENTALS',
+            payload: rentals
+        }))
+    }
 }
 
 //this is our action creator which creates an action object 
