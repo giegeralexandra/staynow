8730586097389568961345ddc7b00ace93ac882f
giegeralexandra <giegeralexandra@gmail.com>
2021-05-28 20:36:55 -0400

    added navbar, added reservations for user only

--- src/components/Rentals.js
+++ src/components/Rentals.js
@@ -2,24 +2,29 @@ import React from 'react'
 import {Route, Link} from 'react-router-dom'
 import Rental from './Rental'
 import {connect} from 'react-redux'
+
 const Rentals = (props) => {
     // debugger
-    // let rentals = props.rentals
     // const [loading, setLoading] = useState(false)
 
     
-    if (!props.rentals[0]){
-        return <p>Rentals loading</p>
-    }
+    // if (!props.rentals[0]){
+    //     return <p>Rentals loading</p>
+    // }
     // debugger
     return (
+        console.log(props),
         <div>
-            {props.rentals.rentals[0].map((rental) => 
+            Rentals
+            {props.rentals.map((rental) => 
             <li key={rental.id}>
                 <Link to={`/rentals/${rental.id}`}>
                     {rental.name}
                 </Link>
             </li>)}
+            <br>
+            </br>
+           
         </div>
     )
 }
