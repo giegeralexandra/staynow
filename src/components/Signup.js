6669ffcdbae10119499e42a1d2efeda75cdbf511
giegeralexandra <giegeralexandra@gmail.com>
2021-05-29 12:30:34 -0400

    fixed bug on reservation entry

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {updateLoginForm} from '../actions/updateLoginForm'
+import {userLogin} from '../actions/setCurrentUser'
 
-const Signup = (props) =>{
+const Signup = (props) => {
 
-    
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        const updatedFormInfo = {
+            ...props.loginForm, 
+            [name]: value
+        }
+        props.updateLoginForm(updatedFormInfo)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.userLogin(props.loginForm)
+        // this.props.setState({
+        //     email: "", 
+        //     password: ""
+        // })
+    }
+
     return (
-        <div className="text-red-700">
-            Signup
-        </div>
+        <form onSubmit={handleSubmit}>
+            <br/><p><input type="text"  name="email" value={props.loginForm.email} onChange={handleChange} placeholder="Email" /></p><br/>
+            <p><input type="text" name="password" value={props.loginForm.password} onChange={handleChange} placeholder="Password" /></p><br/>
+            <p><input type="submit" value="Login"/></p>
+        </form>
     )
 }
 
-export default Signup
+const mapStateToProps = state => {
+    return {
+        loginForm: state.login
+    }
+} 
+
+const mapDispatchToProps = () => {
+    return {
+        updateLoginForm: updateLoginForm,
+        userLogin: userLogin
+    }
+  }
+
+export default connect(mapStateToProps, {userLogin, updateLoginForm})(Login)
